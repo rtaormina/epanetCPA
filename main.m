5ac7970eac8f7311eb74a3698a324c93cc5e8ad6
Riccardo Taormina <riccardo.taormina@gmail.com>
2018-10-16 11:55:42 +0800

    Add minor changes to .cpa headers

--- main.m
+++ main.m
@@ -7,20 +7,20 @@ clear; clc;
 addpath('.\epanetCPA\')
 
 % add location of the map and cpa files  
-inpFilePath = 'ctown_map.inp';
+inpFilePath = 'minitown_map.inp';
 
 % Define scenario
-scenarioFolder = './scenarios/ctown/';
-cpaFilePath = 'scenario05.cpa';
-noAttackCpaFile = 'no_attacks.cpa';
+scenarioFolder = './scenarios/minitown/';
+cpaFilePath = 'minitown_attack.cpa';
+noAttackCpaFile = 'minitown_no_attacks.cpa';
 exp_name = cpaFilePath(1:strfind(cpaFilePath,'.cpa')-1);
 % 
-% % 
-% % Similation without attacks (used for comparison).
-% % It is slower we specified to store all EPANET variables.
-% simul = EpanetCPA(inpFilePath, [scenarioFolder, noAttackCpaFile]); % 
-% simul = simul.run();
-% simul.outputResults('no_attacks');
+% 
+% Similation without attacks (used for comparison).
+% It is slower we specified to store all EPANET variables.
+simul = EpanetCPA(inpFilePath, [scenarioFolder, noAttackCpaFile]); % 
+simul = simul.run();
+simul.outputResults('no_attacks');
 
 % Similation with attacks
 simul = EpanetCPA(inpFilePath, [scenarioFolder,cpaFilePath]); % 
